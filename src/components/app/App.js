395198b37a055c13e99422af066ae28e57565213
Filip Persson <filip.tillberg.persson@gmail.com>
2024-08-27 17:00:33 +0200

    initial commit after clone

---
+++ src/components/app/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from '../header/Header';
+import Home from '../home/Home';
+import BusinessCard from '../contact/BusinessCard';
+import './app.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<BusinessCard />} />
+            <Route path="*" element={<NotFound />} /> {/* Catch-all route */}
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+function NotFound() {
+  return (
+    <h1 style={{color: "white", marginLeft: "50px"}}>404 - Page Not Found</h1>
+  )
+}
+
+export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,53 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './header.css';
+import Logo from '../../images/logo.svg';
+
+function Header() {
+  const [dropdownOpen, setDropdownOpen] = useState(false)
+  const dropdownRef = useRef(null);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen)
+  }
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setDropdownOpen(false);
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <header>
+      <nav className="nav">
+        <div className="nav-logo-div">
+          <img className="logo" src={Logo} alt="logo"></img>
+          <h3>ReactFacts</h3>
+        </div>
+        <ul className="navbar-menu">
+          <li className="navbar-item dropdown" ref={dropdownRef}>
+            <button onClick={toggleDropdown} className="dropdown-toggle">
+              Menu
+            </button>
+            {dropdownOpen && (
+              <ul className={`dropdown-menu ${dropdownOpen ? 'open' : ''}`}>
+                <li className="dropdown-item"><Link to="/">Home</Link></li>
+                <li className="dropdown-item"><Link to="/contact">Contact</Link></li>
+                <li className="dropdown-item"><Link to="#">About</Link></li>
+              </ul>
+            )}
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
